8358666f05d5a97e4c0d579f3a095793e4c56a32
kashaf <kashaf9948@gmail.com>
2026-08-08 13:14:10 +0500

    UPDATE bid-checkout  screen

--- lib/screens/buyer/bid_checkout_screen.dart
+++ lib/screens/buyer/bid_checkout_screen.dart
@@ -3,10 +3,11 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import '../../core/services/product_service.dart';
 import '../../core/utils/theme.dart';
+import '../../core/widgets/client_bottom_nav.dart';
 import '../../core/widgets/snackbars.dart';
 
 class BidCheckoutScreen extends StatefulWidget {
-  const BidCheckoutScreen({super.key});
+  const BidCheckoutScreen({Key? key}) : super(key: key);
 
   @override
   State<BidCheckoutScreen> createState() => _BidCheckoutScreenState();
